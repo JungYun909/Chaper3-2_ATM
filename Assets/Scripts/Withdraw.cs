cb2da87a6759fc1d210d8df43dac941aebe602a2
JungYun909 <jungyun909@gmail.com>
2023-12-13 17:03:08 +0900

    [feat]출금 시스템 구현

--- Assets/Scripts/Withdraw.cs
+++ Assets/Scripts/Withdraw.cs
@@ -1,18 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Withdraw : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int WithdrawMoney;
+    public InputField MoneyInput;
+    public GameObject FailPanel;
+
+    private void Awake()
     {
-        
+        WithdrawMoney = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    void WithdrawCheck()
     {
-        
+        if (GameManager.I.Banlance >= WithdrawMoney)
+        {
+            GameManager.I.Banlance -= WithdrawMoney;
+            GameManager.I.Money += WithdrawMoney;
+
+        }
+
+        else
+        {
+            FailPanel.SetActive(true);
+
+        }
+    }
+
+    public void WithdrawBT1()
+    {
+        WithdrawMoney = 10000;
+        WithdrawCheck();
+
+    }
+
+    public void WithdrawBT2()
+    {
+        WithdrawMoney = 30000;
+        WithdrawCheck();
+    }
+
+    public void WithdrawBT3()
+    {
+        WithdrawMoney = 50000;
+        WithdrawCheck();
+    }
+
+    public void WithdrawBT4()
+    {
+        WithdrawMoney = Convert.ToInt32(MoneyInput.GetComponent<InputField>().text);
+        WithdrawCheck();
+        MoneyInput.GetComponent<InputField>().text = "";
     }
 }
